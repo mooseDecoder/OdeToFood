60092ecde4c756df639009ded5715569280614ed
Moose Decoder <moosedecoder@outlook.com>
2018-02-25 18:36:00 -0800

    Module 3

--- OdeToFood/Program.cs
+++ OdeToFood/Program.cs
@@ -17,6 +17,15 @@ namespace OdeToFood
             BuildWebHost(args).Run();
         }
 
+        //quick and easy way to spin up a webhost via the CreateDefaultBuilder method.
+        //A WebHostBuilder is a an object that knows how to set up our web server environment, and the default web host
+        //builders sets up our environment in a specific way - we change by writing our own defaultbuidler code.
+        //CreateDefaultBuilder does the following:
+        //1. Use the Kestrel server, the cross platform web server. Listens for http connections.
+        //2. Sets up IIS integration -- passes Windows credentials, important for if you are setting up Intranet apps inside fw.
+        //3. Default Logging
+        //4. IConfiguration service made available. This runs throughout the application and is the way to retreive configuration 
+        // information through it. Can pass through appsettings.json. Create this file via the ASP.NET Configuration file template. bb
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -5,29 +5,46 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace OdeToFood
 {
+    //purpose of Startup class is so you can reigster your custom services so .net core can use
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //lifetime of services: Singleton: only need one instance for entire app, Transient: if an interface is needed, create
+            //a new instance, Scoped: create a service that is scoped to a spcific request and is reused in that request then discarded.
+
+            //This basically says: When someone asks for service that implements IGreeter, use an instance of the Greeter class.
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IConfiguration configuration, IGreeter greeter)
         {
             if (env.IsDevelopment())
             {
+                
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler();
+            }
 
+
+
+            //this code runs on every request that is recieved.
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                throw new Exception("error");
+                var greeting = greeter.GetMessageOfTheDay();
+                await context.Response.WriteAsync(greeting);
             });
         }
     }
